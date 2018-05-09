054621931e447955ba6aa870afda5d5084450297
Jaredm95 <Jaredm95@users.noreply.github.com>
2018-05-08 19:48:31 -0700

    Changed Search Value Checking
    Added a loop for replacing spaces with dashes on the value that is searched.

--- main.js
+++ main.js
@@ -24,8 +24,11 @@ function callingPokemon() {
 
 // function for searching for the Pokemon
 function searchingForPoke(poke) {
+    // Loop for checking and replacing the spaces of a search with dashes
+    for (let i = 0; i < poke.length; i++) {
+        poke = poke.replace(" ", "-");
+    } // End of loop
     let pokemon = poke.toLowerCase(); // Variable for the Pokemon name that was searched
-
     let pokeObject = callingPokemon(); // Calling the Function that calls the Pokemon API
     let x = 0;
     // Loop for searching the results of the API call and comparing them to the search
@@ -58,15 +61,15 @@ function initPokemon() {
 
 // Function for populating the page with information on the searched pokemon
 function loadingPage(pokeObject) {
-    
+
     let pokemonVersion = pokeObject.species.url; // Variable for the URL of the Pokemon Species
     let pokemon = gettingMoreInfo(pokemonVersion); // Calling the Function that Calls the API with the URL for the Pokemon Species
 
     let pokemonEvo = pokemon.evolution_chain.url; // Variable for the URL of the Pokemon Evolution 
     let evolution = gettingMoreInfo(pokemonEvo); // Calling the Function that calls the API with the URL for the Pokemon Evolution
-    
+
     /* Images */
-    
+
     let spriteOne = document.getElementById("spriteOne"); // Variable for the first picture
     let spriteTwo = document.getElementById("spriteTwo"); // Variable for the second picture
     let spriteThree = document.getElementById("spriteThree"); // Variable for the third picture
@@ -75,22 +78,22 @@ function loadingPage(pokeObject) {
     spriteOne.src = pokeObject.sprites.front_default; // Pulling in image one
     spriteTwo.src = pokeObject.sprites.front_shiny; // pulling in image two
     spriteThree.src = pokeObject.sprites.back_default; // pulling in image three
-    spriteFour.src = pokeObject.sprites.front_shiny; // Pulling in image four
+    spriteFour.src = pokeObject.sprites.back_shiny; // Pulling in image four
 
     /* Title */
-    
+
     let title = document.getElementById("title") // variable for the main title of the page
 
     let pokeName = pokeObject.name; // Variable for the Pokemon Name
 
     title.innerHTML = pokeName.charAt(0).toUpperCase() + pokeName.slice(1); // Displaying the Name of the Pokemon Searched, with the first letter uppercase.
-    
+
     /* Description */
-    
+
     document.getElementById("desc").innerHTML = pokemon.flavor_text_entries[1].flavor_text; // Displaying the EN Description of the Pokemon
-    
-    /* Moves */ 
-    
+
+    /* Moves */
+
     let moveset = document.getElementById("moves"); // Variable for the moveset section of the page
 
     // Loop for displaying the moveset of the Pokemon
@@ -99,20 +102,41 @@ function loadingPage(pokeObject) {
         pokeMove.innerHTML = pokeObject.moves[x].move.name; // Providing the move name to the p tag
         moveset.appendChild(pokeMove); // displaying the p tag in the moveset section
     } // Loop ends when the moves run out
-    
+
     /* Stats */
-    
+
     let stats = document.getElementById("stats"); // Variable for the stats section
 
     // Loop for displaying the Stats
     for (let x = 0; x < pokeObject.stats.length; x++) {
-        let pokeStat = document.createElement("li");
-        pokeStat.innerHTML = pokeObject.stats[x].stat.name + ": " + pokeObject.stats[x].base_stat;
+        let pokeStat = document.createElement("li"); // Variable for the List Items 
+        pokeStat.innerHTML = pokeObject.stats[x].stat.name + ": " + pokeObject.stats[x].base_stat; // Filling the List items with 
 
-        stats.appendChild(pokeStat);
+        stats.appendChild(pokeStat); // Displaying 
     } // Loop ends when stats runs out
-    
+
     /* Evolution */
+
+    if (evolution.chain.evolves_to.length > 0) {
+        document.getElementById("evoTitle").style.display = "inline";
+        let evoList = document.getElementById("evo");
+        /* Starting Pokemon */
+        let baseState = document.createElement("li");
+        baseState.innerHTML = evolution.chain.species.name;
+        evoList.appendChild(baseState);
+        /* Evolution One */
+        let evoOne = document.createElement("li");
+        evoOne.innerHTML = evolution.chain.evolves_to["0"].species.name;
+        evoList.appendChild(evoOne);
+        /* Evolution Two */
+        if (evolution.chain.evolves_to[0].evolves_to.length > 0) {
+            let evoTwo = document.createElement("li");
+            evoTwo.innerHTML = evolution.chain.evolves_to["0"].evolves_to["0"].species.name;
+            evoList.appendChild(evoTwo);
+        }
+    } else {
+        document.getElementById('evoTitle').style.display = "none";
+    }
 }
 
 // Function for calling the API with new URL's, used fo gathering additional information that isn't in the original JSON object
@@ -124,5 +148,5 @@ function gettingMoreInfo(pokemonID) {
     let pokeObject = JSON.parse(xhr.responseText); // Declaring a variable for the JSON object 
     console.log(pokeObject); // Logging the object
 
-    return pokeObject;
+    return pokeObject; // Returning the JSON data
 }
