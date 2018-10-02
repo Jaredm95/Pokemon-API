062de9b4aa0e9336bca4ebe010064faab7fb151a
Jared <jared97martinez@gmail.com>
2018-10-01 21:15:10 -0700

    Templating Vue Objects

--- js/script.js
+++ js/script.js
@@ -4,21 +4,39 @@ Programmer: Jared Martinez
 Project: Pokemon API Vue JS
 Date: 9/1/2018
 */
-/* Vue App */
+document.addEventListener("DOMContentLoaded", () => {
+    /* Vue App */
 
-// Variables
-const data = {
-    pSearch : '',
-}
+    /* Variables */
 
-// Functions
-const methods = {
+    // Object for storing variables
+    const data = {}
+    data.currentYear = new Date().getFullYear();
+    data.search = '';
 
-}
+    /* Functions */
 
-// Vue Instance
-const pokeApp = new Vue({
-    el: '#pokeApp',
-    data: data, 
-    methods: methods
+    // Object for storing functions
+    const methods = {}
+
+    /* Components */
+
+    // Object fot storing components
+    const components = {}
+
+    components.search = {
+        data: () => {
+            return data;
+        },
+        template: `<input type='text' placeholder='Search here..'>`
+    }
+    // Vue Instance for main app
+    const pokeApp = new Vue({
+        el: '#pokeApp',
+        data: data,
+        methods: methods,
+        components: {
+            'search-area': components.search
+        }
+    });
 });
