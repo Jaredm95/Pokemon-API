16ec9edd986a01f1dc673072728e8b09b2c1686b
Jared <jared97martinez@gmail.com>
2018-10-05 10:30:38 -0700

    Setting up base functions for search

--- js/script.js
+++ js/script.js
@@ -4,32 +4,84 @@ Programmer: Jared Martinez
 Project: Pokemon API Vue JS
 Date: 9/1/2018
 */
-document.addEventListener("DOMContentLoaded", () => {
+
+// Event listener for Content loaded
+$(() => {
     /* Vue App */
 
     /* Variables */
 
-    // Object for storing variables
+    // Object for variables
     const data = {}
     data.currentYear = new Date().getFullYear();
-    data.search = '';
+    data.search = ''; 
+    data.pokemon = '';
+    data.placeHolder = '';
 
     /* Functions */
 
-    // Object for storing functions
+    // Object for functions
     const methods = {}
+
+    // Function for checking user search
+    methods.userSearch = () => {
+        (data.search.length != 0)? methods.findSearch(): methods.badSearch();
+    }
+
+    methods.findSearch = () => {
+        for(let i = 0; i < data.pokemon.results.length; i++){
+            if(data.search == data.pokemon.results[i].name){
+                methods.foundPokemon(data.pokemon.results[i].url);
+                break;
+            } 
+        }
+    }
+
+    methods.getPokemon = () => {
+        $.getJSON('https://pokeapi.co/api/v2/pokemon/', (response) => {
+            data.pokemon = response; 
+        });
+    }
+
+    methods.apiCall = (url) => {
+        console.log(url);
+        $.getJSON(url, (res)  => {
+            console.log(res);
+            data.placeHolder = res;
+        });
+    }
+
+    methods.foundPokemon = (url) => {
+        console.log(url);
+        methods.apiCall(url);
+        while(data.placeHolder == ''){
+            console.log('hello');
+        }
+        methods.populatePage();
+    }
+
+    methods.populatePage = () => {
+        console.log(data.pokemon);
+    }
 
     /* Components */
 
-    // Object fot storing components
+    // Object for components
     const components = {}
 
     components.search = {
         data: () => {
             return data;
         },
-        template: `<input type='text' placeholder='Search here..'>`
+        methods: methods,
+        template: `
+        <div>
+            <input type="text" placeholder="Search here.." v-model="search">
+            <button v-on:click="userSearch()">Search</button>
+        </div>
+        `
     }
+
     // Vue Instance for main app
     const pokeApp = new Vue({
         el: '#pokeApp',
@@ -37,6 +89,7 @@ document.addEventListener("DOMContentLoaded", () => {
         methods: methods,
         components: {
             'search-area': components.search
-        }
+        },
+        mounted: methods.getPokemon()
     });
 });
